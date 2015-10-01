8a35679b01f93c63c0893ffe8749b6b4ffdb7fa7
Anderson Zanichelli <anderzanic@gmail.com>
2015-10-01 00:48:16 -0300

    Fisrt commit, adapting de project to tab project

---
+++ www/js/controllers.js
@@ -0,0 +1,291 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope, $http, ConfigService) {
+
+  $scope.host = ConfigService.load();
+  $scope.message = '';
+  $scope.success = '';
+  $scope.emailUsed = false;
+
+  $scope.confirmation = function(){
+    var pattern = /^[A-Za-z0-9]\w{6,12}$/;
+    if(invalidadFields($scope)) {
+      $scope.message = "Please, fill all fields.";
+      return;
+    }
+
+    if($scope.password.match(pattern)) {
+      if($scope.password === $scope.confirm) {
+        $scope.message = '';
+        var user = {
+          "name": $scope.name,
+          "email": $scope.email,
+          "password": CryptoJS.SHA256($scope.password).toString()
+        };
+
+        if($scope.emailUsed) {
+          $scope.message = "Email not available, it has been used.";
+          return;
+        }
+
+        $scope.signup(user);
+      } else {
+        $scope.message = "The passwords didn't match";
+      }
+    } else
+      $scope.message = "Please, use a password with numbers, letters and more than 6 chars"
+  };
+
+  $scope.signup = function(user){
+    
+    var promisse = $http.get($scope.host + '/signup/' + user['name'] + '/' + user['email'] + '/' + user['password']);
+    
+    promisse.success( function(data){
+      if(data["insert"] == true){
+        $scope.success = 'Success on save the user.';
+      }
+    }).error(function(error){
+      $scope.message = 'Error on trying to save the user.';
+    });
+  };
+
+  $scope.validateEmail = function(){
+    if($scope.email !== '') {
+      $scope.loading = true;
+
+      var promisse = $http({method: 'get', url: $scope.host + '/email/' + $scope.email});
+
+      promisse.success(function(data){
+        if(emailUsed(data, $scope.email)) {
+          $scope.emailUsed = true;
+          $scope.message = "Email not available, it has been used.";
+        } else {
+          $scope.emailUsed = false;
+          $scope.message = "";
+        }
+
+        $scope.loading = false;
+      }).error(function(error){
+        $scope.message = 'Error on connect to server.';
+        $scope.loading = false;
+      });
+    }
+  };
+
+  function invalidadFields(scope){
+  return  scope.password === undefined || scope.password === '' ||
+          scope.email === undefined || scope.email === '' ||
+          scope.name === undefined || scope.name === '';
+  };
+
+  function emailUsed(data, email) {
+    return data.length > 0 && data[0].email === email;
+  };
+})
+
+.controller('ServicesCtrl', function($scope, $http, ConfigService, PreferencesService){
+  
+  $scope.init = function(){
+    $scope.data = {};
+    var host = ConfigService.load();
+
+    if(host !== '') {
+      var promisse = $http({method: 'GET', url: host + '/types'});
+
+      promisse.success(function(data){
+        $scope.data.types = data;
+      }).error(function(error){
+        $scope.data.error = 'Error on connect to server.';
+      });
+    } else {
+      $scope.data.error = 'Error on connect to server.';
+    }
+  };
+
+  $scope.config = function(type, params){
+      PreferencesService.configuration(type, params);
+  };
+})
+
+.controller('PreferencesCtrl', function($scope, ConfigService){
+  $scope.host = ConfigService.load();
+})
+
+.service('PreferencesService', function(){
+  var data = {};
+
+  var config = function(service, params) {
+    data.service = service;
+    data.params = params;
+  };
+
+  var params = function(){
+    return data.params;
+  };
+
+  var service = function() {
+    return data.service;
+  };
+
+  return {
+    configuration: config,
+    getParams: params,
+    getService: service 
+  };
+})
+
+.directive('preferences', function(){
+  return {
+    restrict: 'E',
+    replace: true,
+    templateUrl:'templates/prefs.html',
+    controller: function ($scope, $http, ConfigService, PreferencesService) {
+    $scope.host = ConfigService.load();
+    //var email = configService.recoverEmail();
+
+    $scope.savePreferences = function(){
+      var promisse = $http.post($scope.host + '/savePreferences', $scope.amount);
+
+      promisse.success(function(result){
+        console.log(result);
+      });
+
+      promisse.error(function(error){
+        console.log(error);
+      });
+    };
+
+    $scope.create = function(){
+      $scope.prefs = {};
+
+      var div = $('<div>');
+
+      var user = $('<input>');
+      user.attr('type', 'hidden');
+      user.attr('name', 'user');
+      user.attr('value', ConfigService.recoverEmail());
+
+      var service = $('<input>');
+      service.attr('type', 'hidden');
+      service.attr('name', 'service');
+      service.attr('value', PreferencesService.getService());
+
+      div.append(user);
+      div.append(service);
+
+      $.each(PreferencesService.getParams(), function (idx, obj) {
+        var container = $('<div>');
+        container.attr('id', 'container');
+
+        var el = $('<' + obj.element + '>');
+        el.attr('type', obj.type);
+        el.attr('name', obj.name);
+
+        $scope.prefs[obj.name];
+
+        if(obj.type === "number") {
+          el.addClass("number");
+        }
+
+        var label = $('<label>');
+
+        if (obj.type === "checkbox") {
+          label.addClass('toogle');
+          label.append(obj.label);
+          container.append(el);
+          container.append(label);
+
+        } else {
+          el.attr('placeholder', obj.label);
+          label.addClass('item item-input');
+          label.append(el);
+          container.append(label);
+        }
+
+        if (obj.type === "select") {
+          for (var item in obj.content) {
+            var option = $('<option>');
+            option.val(item);
+            option.html(obj.content[item]);
+            el.append(option);
+          }
+        }
+
+        div.append(container);
+      });
+
+      $('#preferencia').html(div.html());
+      }
+    }
+  };
+})
+
+.controller('ConfigCtrl', function($scope, ConfigService) {
+  
+  $scope.save = function() {
+    var config = {
+      address: $scope.address,
+      port: $scope.port
+    };
+  
+    ConfigService.save(config);
+    $scope.load();
+  };
+
+  $scope.load = function() {
+    $scope.host = ConfigService.load();
+  };
+
+  $scope.load();
+})
+
+.controller('LoginCtrl', function($scope, $http, ConfigService) {
+  $scope.host = ConfigService.load();
+  $scope.message = '';
+
+  $scope.login = function() {
+    //$scope.data.loggedIn = true;
+    if ($scope.email && $scope.password) {
+      $scope.loading = true;
+      var user = {
+        "email": $scope.email,
+        "password": CryptoJS.SHA256($scope.password).toString()
+      };
+
+      var promisse = $http.get($scope.host + '/login/' + '/' + user['email'] + '/' + user['password']);
+
+      promisse.success(function (data) {
+        $scope.loading = false;
+        if (data.logged){
+          $scope.data.loggedIn = true;
+          ConfigService.storeEmail($scope.email);
+        } else
+          $scope.message = 'User or password did not match';
+      }).error(function (error) {
+        $scope.message = 'Error on login.';
+        $scope.loading = false;
+      });
+    }
+  }
+});
---
+++ www/js/services.js
@@ -0,0 +1,101 @@
+angular.module('starter.services', [])
+
+.factory('ConfigService', function(){
+  
+  var storeEmail = function(email) {
+    window.localStorage.setItem('email', email);
+  }
+
+  var recoverEmail = function() {
+    return window.localStorage.getItem('email');
+  }
+
+  var save = function(config){
+    window.localStorage.setItem('brokerconfig', JSON.stringify(config));
+  };
+
+  var load = function() {
+    var host = '';
+    var config = JSON.parse(window.localStorage.getItem('brokerconfig'));
+
+    if(config !== null && config.address !== '' && config.address !== undefined) {
+      if(temHttp(config.address))
+        host = config.address;
+      else {
+        var number = parseInt(config.address.substring(0,1));
+        if(isNaN(number)) {
+          host = 'http://' + config.address;
+        } else {
+          host = config.address;
+        }
+      }
+
+      if(config.port) {
+        if(config.port !== undefined && config.port > 0)
+          host = host + ":" + config.port;
+        }
+      }
+
+      return host;
+  };
+
+  function temHttp(address){
+    return address.substring(0,7) === 'http://' || address.substring(0,8) === 'https://';
+  };
+
+  return {
+    save: save,
+    load: load,
+    storeEmail: storeEmail,
+    recoverEmail: recoverEmail
+  };
+})
+
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+  }, {
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+  }];
+
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+});
